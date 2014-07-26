15eb93fd373a2be3c7df6ba735f6b7b14d78b5d5
drak@kaverne <drak@kaverne>
2014-07-26 17:15:00 +0200

    start bare skin.

--- src/plugins/hello/MyApplication.java
+++ src/plugins/hello/MyApplication.java
@@ -18,4 +18,31 @@ public class MyApplication implements FredPlugin {
     
     public void terminate()
     {}
+
+    private void setupWebInterface()
+    {
+        PluginContext pluginContext = new PluginContext(pr);
+        this.webInterface = new WebInterface(pluginContext);
+        
+        pr.getPageMaker().addNavigationCategory(basePath + "/","WebOfTrust.menuName.name", "WebOfTrust.menuName.tooltip", this);
+        ToadletContainer tc = pr.getToadletContainer();
+        
+        // pages
+        Overview oc = new Overview(this, pr.getHLSimpleClient(), basePath, db);
+        
+        // create fproxy menu items
+        tc.register(oc, "WebOfTrust.menuName.name", basePath + "/", true, "WebOfTrust.mainPage", "WebOfTrust.mainPage.tooltip", WebOfTrust.allowFullAccessOnly, oc);
+        tc.register(oc, null, basePath + "/", true, WebOfTrust.allowFullAccessOnly);
+        
+        // register other toadlets without link in menu but as first item to check
+        // so it also works for paths which are included in the above menu links.
+        // full access only will be checked inside the specific toadlet
+        for(Toadlet curToad : newToadlets) {
+            tc.register(curToad, null, curToad.path(), true, false);
+        }
+        
+        // finally add toadlets which have been registered within the menu to our list
+        newToadlets.add(oc);
+    }
 }
+
