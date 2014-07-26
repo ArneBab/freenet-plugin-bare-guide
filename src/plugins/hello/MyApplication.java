beb1959e135b507711d71d586fa134cf24779513
drak@kaverne <drak@kaverne>
2014-07-26 11:58:00 +0200

    java for minimal application written.

---
+++ src/plugins/hello/MyApplication.java
@@ -0,0 +1,12 @@
+package hello.world
+
+    public class MyApplication extends FredPlugin {
+    
+    private final static Logger LOGGER = Logger.getLogger(MyApplication.class.getName());
+    
+    public void runPlugin(PluginRespirator pr)
+    {}
+    
+    public void terminate()
+    {}
+}
